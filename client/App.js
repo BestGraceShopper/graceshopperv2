0141b7658d0feaecd0c0be521a3c25316ebb0a0c
Justin <mintzer.justin@gmail.com>
2019-02-18 14:32:35 -0500

    Installed react-bootstrap and made a navbar

--- client/App.js
+++ client/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Routes from './Routes';
 
+import Navbar from './components/Navbar';
+
 export default class App extends React.Component {
   render() {
     return (
       <div>
+        <Navbar />
         <Routes />
       </div>
     );
---
+++ client/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+
+class Navigationbar extends React.Component {
+  render() {
+    return (
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Shopper</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#products">Products</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+    );
+  }
+}
+
+export default Navigationbar;
