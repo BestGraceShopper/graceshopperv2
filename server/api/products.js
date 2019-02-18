c5398e3e726e90f66fed95d80e580f3d01f8f6d3
Justin <mintzer.justin@gmail.com>
2019-02-18 13:26:35 -0500

    getById is deprecated, now using getByPk

--- server/api/products.js
+++ server/api/products.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const singleProduct = await Product.findById(req.params.id);
+    const singleProduct = await Product.findByPk(req.params.id);
     res.json(singleProduct);
   } catch (err) {
     next(err);
